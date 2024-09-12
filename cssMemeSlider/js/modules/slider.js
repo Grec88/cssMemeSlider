5660c697b74b62092f7f29b3a577718fa647de47
grec88 <Lihtinmann88@yandex.ru>
2024-09-12 20:08:16 +0300

    feat: add slider functionality (Thu, Sep 12, 2024 08:08:16 PM)

--- cssMemeSlider/js/modules/slider.js
+++ cssMemeSlider/js/modules/slider.js
@@ -2,11 +2,27 @@ export const slider = () => {
   console.log(1)
   const cards = document.querySelector('.slider__cards');
   const buttons = document.querySelectorAll('.slider__control');
+  let currentCard = 0;
 
-  const openCard = () => {
+  const openCard = (event) => {
+    const cardWidth = cards.offsetWidth;
     console.log(1)
-    cards.style.marginLeft = `${-700}px`;
+    currentCard = event.target.id;
+    console.log(cardWidth)
+    console.log(currentCard)
+
+    cards.classList.add('animate-margin');
+    cards.style.marginLeft = `${-cardWidth * (currentCard)}px`;
+  }
+
+  const changeLeftMargin = () => {
+    const cardWidth = cards.offsetWidth;
+    console.log(cardWidth)
+
+    cards.classList.remove('animate-margin');
+    cards.style.marginLeft = `${-cardWidth * (currentCard)}px`;
   }
 
   buttons.forEach(button => button.addEventListener('click', openCard));
+  window.addEventListener('resize', changeLeftMargin);
 }
