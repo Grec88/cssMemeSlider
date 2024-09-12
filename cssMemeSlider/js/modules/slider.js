50bdefb20883248fc73865f4eaecc1aad82ea113
grec88 <Lihtinmann88@yandex.ru>
2024-09-12 20:25:23 +0300

    feat: add text transitioned change (Thu, Sep 12, 2024 08:25:23 PM)

--- cssMemeSlider/js/modules/slider.js
+++ cssMemeSlider/js/modules/slider.js
@@ -1,21 +1,35 @@
+import { memeInfo } from "../../assets/data/memeInfo.js";
+
 export const slider = () => {
   console.log(1)
   const cards = document.querySelector('.slider__cards');
   const buttons = document.querySelectorAll('.slider__control');
+  const cardsDescription = document.querySelector('.slider__description');
   let currentCard = 0;
 
+  const changeSliderDesc = (currentCard) => {
+
+    cardsDescription.classList.add('hidden');
+    setTimeout(() => {
+      cardsDescription.textContent = `${memeInfo[currentCard]}`;
+      cardsDescription.classList.remove('hidden');
+    }, 500);
+  }
+
   const openCard = (event) => {
+
     const cardWidth = cards.offsetWidth;
     console.log(1)
     currentCard = event.target.id;
     console.log(cardWidth)
     console.log(currentCard)
-
+    changeSliderDesc(currentCard)
     cards.classList.add('animate-margin');
     cards.style.marginLeft = `${-cardWidth * (currentCard)}px`;
   }
 
   const changeLeftMargin = () => {
+    
     const cardWidth = cards.offsetWidth;
     console.log(cardWidth)
 
